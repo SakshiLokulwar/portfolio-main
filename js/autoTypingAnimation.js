937da295492410f4f526f9ec958ec2b1cc733c39
Sakshi Lokulwar <sakshilokulwar@gmail.com>
2024-08-30 21:54:43 +0530

    Initial commit

---
+++ js/autoTypingAnimation.js
@@ -0,0 +1,9 @@
+setTimeout(function () {
+  $(".about-info-inner").typed({
+    strings: [
+      "<span>Hi, I am Sakshi Lokulwar</span> , a dedicated web developer driven by a profound passion for software development and project management. With a background in Master of Computer Application from Dy Patil School of Mca Lohegaon, Pune, I have honed my skills to merge theoretical knowledge with practical applications. My journey as a web developer is rooted in a fervent drive to innovate and create meaningful solutions. Embracing challenges with enthusiasm, I thrive on the dynamic intersection of technology and creativity. Beyond coding, I am passionate about software development methodologies and project management practices. Leveraging certifications in Agile Project Management and expertise in SQL databases, I bring a holistic approach to my projects, ensuring both technical excellence and efficient execution.",
+    ],
+    typeSpeed: 7,
+    contentType: "html",
+  });
+}, 100);
